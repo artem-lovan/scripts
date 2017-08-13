cd3932bc63669f048974b7ebe678c1738bf48ac3
artem <artem@hedgeable.com>
2017-08-12 23:46:37 -0400

    dev

--- setup-redhat.sh
+++ setup-redhat.sh
@@ -1,13 +1,12 @@
 yes Y | sudo yum update \
-    yes Y | sudo yum install wget \
-    yes Y | sudo yum install java-1.8.0-openjdk \
-    yes Y | sudo yum install java-1.8.0-openjdk-devel \
-    yes Y | sudo yum install tmux
+    && yes Y | sudo yum install wget \
+    && yes Y | sudo yum install java-1.8.0-openjdk \
+    && yes Y | sudo yum install java-1.8.0-openjdk-devel \
+    && yes Y | sudo yum install tmux
 
 # get maven
-wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
-tar xvf apache-maven-3.0.5-bin.tar.gz
-sudo mv apache-maven-3.0.5  /usr/local/apache-maven
-echo "export PATH=$PATH:/usr/local/apache-maven/bin" >> ~/.bashrc
-source ~/.bashrc
-rm -fr apache-maven-3.0.5-bin.tar.gz
+wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz \
+	&& tar xvf apache-maven-3.0.5-bin.tar.gz \
+	&& sudo mv apache-maven-3.0.5  /usr/local/apache-maven \
+	&& echo "export PATH=$PATH:/usr/local/apache-maven/bin" >> ~/.bashrc \
+	&& source ~/.bashrc

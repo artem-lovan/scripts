38fb13977e86119ceb56219845a27f3f9ee19501
artem <artem@hedgeable.com>
2017-08-12 23:48:12 -0400

    dev

--- setup-redhat.sh
+++ setup-redhat.sh
@@ -9,4 +9,5 @@ wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.0.5/binaries/apache-
 	&& tar xvf apache-maven-3.0.5-bin.tar.gz \
 	&& sudo mv apache-maven-3.0.5  /usr/local/apache-maven \
 	&& echo "export PATH=$PATH:/usr/local/apache-maven/bin" >> ~/.bashrc \
-	&& source ~/.bashrc
+	&& source ~/.bashrc \
+    && rm -fr apache-maven-3.0.5-bin.tar.gz

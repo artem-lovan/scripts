2aeb15ea868ac7d47406b25cf4361cec86fa605e
artem <artem@hedgeable.com>
2017-08-28 11:51:43 -0400

    added docker install as per docs

---
+++ setup-docker-docs.sh
@@ -0,0 +1,15 @@
+# install docker as per docs on docker page
+sudo apt-get update -y
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install -y docker-ce=17.06.0~ce-0~ubuntu
+sudo usermod -a -G docker ubuntu

4174a29a06c4e3819f317f96834da0695e862da2
artem <artem@hedgeable.com>
2017-09-19 08:19:36 -0400

    added install-java7-ubuntu.sh

---
+++ install-java7-ubuntu.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && \
+    sudo apt-get install -y software-properties-common python-software-properties && \
+    sudo add-apt-repository -y ppa:openjdk-r/ppa && \
+    sudo apt-get update && \
+    sudo apt-get install -y openjdk-7-jdk openjdk-7-jre
